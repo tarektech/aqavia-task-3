560abbacc5d0b77628e0971d79b852ccdc46ca91
tarek alzein <tarekalzein97@gmail.com>
2026-01-28 12:05:39 +0300

    fixing the nextjs build 1.0.1

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
